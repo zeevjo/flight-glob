37d566dfd5a15f493b9176154f9248da51365671
= <=>
2024-12-03 20:46:28 +0200

    feat: add get country by code

---
+++ utils/getCountryCode.js
@@ -0,0 +1,18 @@
+import { api } from "../api/api.js";
+import { SECRET } from "../secret.js";
+import { paths } from "../constants/paths.js";
+
+export async function getCountryCode(country) {
+  const params = {
+    key: SECRET.API_KEY,
+    nameCountry: country,
+  };
+
+  let countryData = await api.get(paths.countryData, params);
+  if (!countryData.length) {
+    console.error("Error: No country found with the given name.");
+    return;
+  }
+
+  return countryData[0].codeIso2Country;
+}
