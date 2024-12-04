00dcbe68850f8b1ddaa98c7def9fc499f04b6580
= <=>
2024-12-04 12:00:57 +0200

    feat: add filter flights by relevance

--- utils/filterFlightsByDes.js
+++ utils/filterFlightsByDes.js
@@ -5,10 +5,9 @@ import { getCountryCode } from "../utils/getCountryCode.js";
 import { getMainAirPortCode } from "../utils/getMainAirPortCode.js";
 import { getAirPortsByCountry } from "../utils/getAirPortsByCountry.js";
 import { extractAPIataCodes } from "../utils/extractAPIataCodes.js";
+import { filterFutureFlights } from "../utils/filterFutureFlights.js";
 
 export async function filterFlightsByDes(des, flights) {
-  console.log("des", des);
-  console.log("flights", flights);
 
   const countryCode = await getCountryCode(des);
 
@@ -17,8 +16,12 @@ export async function filterFlightsByDes(des, flights) {
   const countryAirPortsIata = extractAPIataCodes(countryAirPorts);
 
   console.log("countryAirPortsIata", countryAirPortsIata);
-  
-  const flightsToDes =  flights.filter(flight => countryAirPortsIata.includes(flight.arrival.iataCode));
 
-  return flightsToDes;
+  const flightsToDes = flights.filter((flight) =>
+    countryAirPortsIata.includes(flight.arrival.iataCode)
+  );
+
+  const futureFlightsToDes = filterFutureFlights(flightsToDes);
+
+  return futureFlightsToDes;
 }
