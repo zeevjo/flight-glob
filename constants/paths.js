58825f252abbb98686f612d23d6755e5cf0f992a
= <=>
2024-12-05 22:56:21 +0200

    wip

--- constants/paths.js
+++ constants/paths.js
@@ -3,5 +3,6 @@ export const paths = {
   countryData: "https://aviation-edge.com/v2/public/countryDatabase",
   airPorts: "https://aviation-edge.com/v2/public/airportDatabase",
   todaysFlights: "https://aviation-edge.com/v2/public/timetable",
-  airPortInfo: "https://aviation-edge.com/v2/public/airportDatabase"
+  airPortInfo: "https://aviation-edge.com/v2/public/airportDatabase",
+  gpt: "https://api.openai.com/v1/chat/completions"
 };
