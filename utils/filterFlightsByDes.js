fdac11add469b3e4275f11e80c2a9809ceafe927
= <=>
2024-12-05 18:06:12 +0200

    wip

--- utils/filterFlightsByDes.js
+++ utils/filterFlightsByDes.js
@@ -24,8 +24,6 @@ export async function filterFlightsByDes(des, flights) {
 
     // console.log('%c futureFlightsToDes', 'color: green; font-weight: bold;', futureFlightsToDes);
 
-    
-
     const haveAllDataFlights = filterFlightsByFields(futureFlightsToDes);
 
     const firstThree = haveAllDataFlights.slice(0, 3);
